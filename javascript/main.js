94abc041ef753002fd2b8dfb6f05736e4a1302a1
hutvbl <32150437+hutvbl@users.noreply.github.com>
2020-06-02 17:17:42 +0700

    Update một số thứ

--- javascript/main.js
+++ javascript/main.js
@@ -1,23 +1,47 @@
-$(function () {
-  $(".progress").each(function () {
-    var value = $(this).attr("data-value");
-    var left = $(this).find(".progress-left .progress-bar");
-    var right = $(this).find(".progress-right .progress-bar");
+// $(function () {
+//   $(".progress").each(function () {
+//     var value = $(this).attr("data-value");
+//     var left = $(this).find(".progress-left .progress-bar");
+//     var right = $(this).find(".progress-right .progress-bar");
 
-    if (value > 0) {
-      if (value <= 50) {
-        right.css("transform", "rotate(" + percentageToDegrees(value) + "deg)");
-      } else {
-        right.css("transform", "rotate(180deg)");
-        left.css(
-          "transform",
-          "rotate(" + percentageToDegrees(value - 50) + "deg)"
-        );
-      }
-    }
-  });
+//     if (value > 0) {
+//       if (value <= 50) {
+//         right.css("transform", "rotate(" + percentageToDegrees(value) + "deg)");
+//       } else {
+//         right.css("transform", "rotate(180deg)");
+//         left.css(
+//           "transform",
+//           "rotate(" + percentageToDegrees(value - 50) + "deg)"
+//         );
+//       }
+//     }
+//   });
 
-  function percentageToDegrees(percentage) {
-    return (percentage / 100) * 360;
-  }
-});
+//   function percentageToDegrees(percentage) {
+//     return (percentage / 100) * 360;
+//   }
+// });
+
+// function progress() {
+//   $(".progress").each(function () {
+//     var value = $(this).attr("data-value");
+//     var left = $(this).find(".progress-left .progress-bar");
+//     var right = $(this).find(".progress-right .progress-bar");
+
+//     if (value > 0) {
+//       if (value <= 50) {
+//         right.css("transform", "rotate(" + percentageToDegrees(value) + "deg)");
+//       } else {
+//         right.css("transform", "rotate(180deg)");
+//         left.css(
+//           "transform",
+//           "rotate(" + percentageToDegrees(value - 50) + "deg)"
+//         );
+//       }
+//     }
+//   });
+
+//   function percentageToDegrees(percentage) {
+//     return (percentage / 100) * 360;
+//   }
+// }
